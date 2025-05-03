c29063f1bbae6fe4f316ba0cbf0e1ab6bb742cd1
r-pears <ryjen.pears@gmail.com>
2025-05-03 16:09:58 +0200

    add read a blob object

--- app/main.js
+++ app/main.js
@@ -34,15 +34,24 @@ function createGitDirectory() {
 }
 
 function readFile() {
-  const blob = fs.readFileSync(
-    path.join(
+  try {
+    const gitObjectPath = path.join(
       process.cwd(),
       ".git",
       "objects",
-      process.argv[4].substring(0, 2),
-      process.argv[4].substring(2)
-    )
-  );
-  const buffer = zlib.unzipSync(blob).toString();
-  process.stdout.write(buffer.substring(buffer.indexOf("\x00") + 1));
+      process.argv[4].slice(0, 2),
+      process.argv[4].slice(2)
+    );
+
+    const compressedBlob = fs.readFileSync(gitObjectPath);
+    const decompressedBlob = zlib.unzipSync(compressedBlob).toString();
+
+    const contentStartIndex = decompressedBlob.indexOf("\x00") + 1;
+    const fileContent = decompressedBlob.substring(contentStartIndex);
+
+    process.stdout.write(fileContent);
+  } catch (error) {
+    console.error("Error reading the file:", error.message);
+    process.exit(1); // Exit with a non-zero status code to indicate failure
+  }
 }
