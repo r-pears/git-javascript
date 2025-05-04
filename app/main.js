04a8550f2e4de83b040999b19ba0c1fb631c9233
r-pears <ryjen.pears@gmail.com>
2025-05-04 21:55:44 +0200

    create blob object

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
-const zlib = require("node:zlib");
+const zlib = require("zlib");
+const crypto = require("crypto");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.error("Logs from your program will appear here!");
@@ -14,6 +15,9 @@ switch (command) {
     break;
   case "cat-file":
     readFile();
+    break;
+  case "hash-object":
+    hashObject(process.argv);
     break;
   default:
     throw new Error(`Unknown command ${command}`);
@@ -53,5 +57,40 @@ function readFile() {
   } catch (error) {
     console.error("Error reading the file:", error.message);
     process.exit(1); // Exit with a non-zero status code to indicate failure
+  }
+}
+
+async function hashObject(arguments) {
+  try {
+    const filePath = arguments.at(-1);
+    const absoluteFilePath = path.resolve(__dirname, filePath);
+
+    const fileContent = await fs.readFile(absoluteFilePath);
+
+    const objectBuffer = Buffer.from(
+      `blob ${fileContent.length}\x00${fileContent.toString()}`
+    );
+
+    const blobData = zlib.deflateSync(objectBuffer);
+
+    const hash = crypto.createHash("sha1").update(blobData).digest("hex");
+
+    const objectFolder = hash.slice(0, 2);
+    const objectFile = hash.slice(2);
+    const objectFolderPath = path.join(
+      __dirname,
+      ".git",
+      "objects",
+      objectFolder
+    );
+
+    await fs.mkdir(objectFolderPath, { recursive: true });
+
+    const objectFilePath = path.join(objectFolderPath, objectFile);
+    await fs.writeFile(objectFilePath, blobData);
+
+    process.stdout.write(hash);
+  } catch (error) {
+    console.error("Error hashing object:", error.message);
   }
 }
